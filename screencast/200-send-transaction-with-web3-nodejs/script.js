587aa890d59aa525e5656326832717ba9d2798ce
yuliu-debond <79855548+yuliu-debond@users.noreply.github.com>
2023-02-15 15:53:28 +0100

    Update script.js

--- screencast/200-send-transaction-with-web3-nodejs/script.js
+++ screencast/200-send-transaction-with-web3-nodejs/script.js
@@ -1,44 +1,51 @@
 const Web3 = require('web3');
-const Provider = require('@truffle/hdwallet-provider');
-const MyContract = require('./build/contracts/MyContract.json');
-const address = '';
-const privateKey = '';
-const infuraUrl = ''; 
+const MyContract = require('./Token.json');
+const address = '0xF19fa96028BbF5A5f5e5Ec4f087173F226E77B6B';
+const privateKey = 'eb1044e397960b7d725a06f6a84e90eabd4dbb0041727718d3bc1fda8f6828f2';
+const infuraUrl = 'https://goerli.infura.io/v3/e437121bd4dd420798ffcd24bd77b272'; 
 
-//Hard way (web3#signTransaction() + web3#sendSignedTransaction())
-const init1 = async () => {
-  const web3 = new Web3(infuraUrl);
-  const networkId = await web3.eth.net.getId();
-  const myContract = new web3.eth.Contract(
-    MyContract.abi,
-    MyContract.networks[networkId].address
-  );
 
-  const tx = myContract.methods.setData(1);
-  const gas = await tx.estimateGas({from: address});
-  const gasPrice = await web3.eth.getGasPrice();
-  const data = tx.encodeABI();
-  const nonce = await web3.eth.getTransactionCount(address);
-
-  const signedTx = await web3.eth.accounts.signTransaction(
-    {
-      to: myContract.options.address, 
-      data,
-      gas,
-      gasPrice,
-      nonce, 
-      chainId: networkId
-    },
-    privateKey
-  );
-  console.log(`Old data value: ${await myContract.methods.data().call()}`);
-  const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
-  console.log(`Transaction hash: ${receipt.transactionHash}`);
-  console.log(`New data value: ${await myContract.methods.data().call()}`);
-}
+const amounts = [];
+const values  = {
+    doi: '',
+    author: '',
+    price: '',
+    currency: '',
+    coAuthors: '',
+    uploadTime: '',
+    sponsor: '',
+    affiliation: '',
+    domain: '',
+    subdomain: '',
+    abstract: '',
+    keywords: '',
+    license: '',
+    filename: '',
+    fileHash: '',
+    fileTitle: '',
+    fileType: '',
+    fileFormat: '',
+    reference: '',
+    version: '',
+    uri: '',
+    dataPrice: '',
+    algorithmPrice: '',
+    articleInfo: [],
+    displayDoi: '',
+    displayPrice: '',
+    displayLicense: '',
+    displayAffiliation: '',
+    displayDomain: '',
+    displaySubDomain: '',
+    displayURI: '',
+    displayDataPrice: '',
+    displayAlgorithmPrice: '',
+    authorToken: 0,
+    coAuthorToken: 0
+  };
 
 //Slightly easier (web3#sendTransaction())
-const init2 = async () => {
+const publishPaper = async (amounts, values) => {
   const web3 = new Web3(infuraUrl);
   const networkId = await web3.eth.net.getId();
   const myContract = new web3.eth.Contract(
@@ -46,8 +53,7 @@ const init2 = async () => {
     MyContract.networks[networkId].address
   );
   web3.eth.accounts.wallet.add(privateKey);
-
-  const tx = myContract.methods.setData(2);
+  const tx = myContract.methods.publishPaper(amounts, values);
   const gas = await tx.estimateGas({from: address});
   const gasPrice = await web3.eth.getGasPrice();
   const data = tx.encodeABI();
@@ -59,33 +65,8 @@ const init2 = async () => {
     gas,
     gasPrice,
     nonce, 
-    chain: 'rinkeby', 
-    hardfork: 'istanbul'
   };
 
-  console.log(`Old data value: ${await myContract.methods.data().call()}`);
+ 
   const receipt = await web3.eth.sendTransaction(txData);
-  console.log(`Transaction hash: ${receipt.transactionHash}`);
-  console.log(`New data value: ${await myContract.methods.data().call()}`);
 }
-
-//Easy way (Web3 + @truffle/hdwallet-provider)
-const init3 = async () => {
-  const provider = new Provider(privateKey, 'https://rinkeby.infura.io/v3/74aa9a15e2524f6980edb8a377301f3c'); 
-  const web3 = new Web3(provider);
-  const networkId = await web3.eth.net.getId();
-  const myContract = new web3.eth.Contract(
-    MyContract.abi,
-    MyContract.networks[networkId].address
-  );
-
-  console.log(await myContract.methods.data().call());
-  console.log(`Old data value: ${await myContract.methods.data().call()}`);
-  const receipt = await myContract.methods.setData(3).send({ from: address });
-  console.log(`Transaction hash: ${receipt.transactionHash}`);
-  console.log(`New data value: ${await myContract.methods.data().call()}`);
-}
-
-init3();
-
-
